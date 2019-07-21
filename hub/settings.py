7dec2ef2c4f0aec160b966eb3e9258e9f90f10bf
GabeF <gfrische@ucsd.edu>
2019-07-21 10:40:45 -0700

    Created webapp django app. Connected to settings.py, which I also configured static root and static files dirs.

--- hub/settings.py
+++ hub/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'webapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
 
 try:
     from local_settings import *
